0dd19b33f6006fc8824f597be620534e36f72d18
Allen Golan <agolan@pdx.edu>
2021-02-01 00:45:30 -0800

    bugfix: calculateChange didn't include logic for quarters, now it does.

--- hw2/02-coins.js
+++ hw2/02-coins.js
@@ -4,13 +4,19 @@ function calculateChange() {
   var coinOutputDiv = document.getElementById('coinOutputDiv');
   coinOutputDiv.innerHTML = "";
   var dollarCoinCount = 0;
+  var quarterCount = 0;
   var dimeCount = 0;
   var nickelCount = 0;
   var pennyCount = 0;
   
+//find largest denomination coin count for user entered dollar figure
   while(dollarAmount >= 1){
 	dollarCoinCount++;
 	dollarAmount--;
+  }
+    while(dollarAmount >= 0.25){
+	quarterCount++;
+	dollarAmount-= 0.25;
   }
     while(dollarAmount >= 0.1){
 	dimeCount++;
@@ -25,8 +31,13 @@ function calculateChange() {
 	dollarAmount-= 0.01;
   }
   
+  //print results to the user screen
   coinOutputDiv.innerHTML += "Dollar Coin Count: ";
   coinOutputDiv.innerHTML += dollarCoinCount;
+  coinOutputDiv.innerHTML += "<br />";
+
+  coinOutputDiv.innerHTML += "Quarter Count: ";
+  coinOutputDiv.innerHTML += quarterCount;
   coinOutputDiv.innerHTML += "<br />";
   
   coinOutputDiv.innerHTML += "Dime Count: ";
