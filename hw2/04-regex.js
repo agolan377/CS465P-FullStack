f52d1298c2f85aa68b2ea6a058a6e0c17e6ab5d1
Allen Golan <agolan@pdx.edu>
2021-02-01 10:59:59 -0800

    regex working

--- hw2/04-regex.js
+++ hw2/04-regex.js
@@ -1,1 +1,16 @@
 // Enter your code here
+function validateEmailFormat(){
+    var errorDiv = document.getElementById('errorDiv');
+    var successDiv = document.getElementById('successDiv');
+    successDiv.innerText = "";
+    errorDiv.innerText = "";
+    const email = document.getElementById("input").value;
+    const regex = /[A-Za-z0-9A-Za-z0-9]{1,64}[\@][A-Za-z0-9A-Za-z0-9]{1,255}[\.][a-z]{3}/;
+    const validEmail = email.match(regex);
+    if(validEmail === null || email !==  validEmail[0]){
+        errorDiv.innerText = "Error: Please enter a valid email address.";
+    }
+    else{
+        successDiv.innerText = "Thank you.  This is a valid email address."
+    }
+}
