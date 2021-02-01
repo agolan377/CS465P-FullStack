ea58910d92b089437ffef7980b05bcc417ddc53d
Allen Golan <agolan@pdx.edu>
2021-02-01 02:35:40 -0800

    reverse working

--- hw2/01-fizzbuzz.js
+++ hw2/01-fizzbuzz.js
@@ -1,14 +1,20 @@
+window.onload = function(){
+	fizzbuzz();
+}
+
 function fizzbuzz() {
   // Enter your code here
   var fizzBuzzDiv = document.getElementById('fizzBuzzDiv');
 
   for(var i = 0; i < 100; i++){
-    if(i%3 == 0 && i%5 != 0)
-      fizzBuzzDiv.innerHTML += "fizz ";
-    if(i%3 != 0 && i%5 == 0)
-      fizzBuzzDiv.innerHTML += "buzz ";
-    if(i%3 == 0 && i%5 == 0)
-      fizzBuzzDiv.innerHTML += "fizzbuzz "
+    if(i%3 == 0 && i%5 != 0){
+	  fizzBuzzDiv.innerHTML += " fizz ";
+	}
+    if(i%3 != 0 && i%5 == 0){
+      fizzBuzzDiv.innerHTML += " buzz ";
+	}
+    if(i%3 == 0 && i%5 == 0){
+      fizzBuzzDiv.innerHTML += " fizzbuzz "
+	}
   }
-
 }
--- hw2/03-reverse.js
+++ hw2/03-reverse.js
@@ -1,1 +1,30 @@
 // Enter your code here
+function reverseNum(){
+    var errorDiv = document.getElementById('errorDiv');
+    errorDiv.innerText = "";
+    outputDiv.innerText = "";
+    var userInputedNumber = Number(document.getElementById('input').value);
+    userInputedNumber.value = 45;
+    if(userInputedNumber < 100000000 && userInputedNumber > 9999999){
+        outputDiv.innerText += userInputedNumber;
+        outputDiv.innerText += " -> "
+        outputDiv.innerText += reverseHelper(userInputedNumber);
+    }
+    else{
+        errorDiv.innerText = "Error: Please enter an 8 digit number.";
+    }
+}
+
+//reverseHelper() adapted from code found at
+//https://www.freecodecamp.org/news/js-basics-how-to-reverse-a-number-9aefc20afa8d/
+function reverseHelper(num) {
+    return (
+      parseFloat(
+        num
+          .toString()
+          .split('')
+          .reverse()
+          .join('')
+      )
+    )                 
+  }
