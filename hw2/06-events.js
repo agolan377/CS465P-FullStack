5290671d6c0403394b98229ae7ebc8c90fdc90ba
Allen Golan <agolan@pdx.edu>
2021-02-01 17:41:12 -0800

    events working

--- hw2/06-events.js
+++ hw2/06-events.js
@@ -1,1 +1,10 @@
 // Enter your code here
+window.onload = windowSizeChange;
+window.onresize = windowSizeChange;
+
+function windowSizeChange(){
+    var windowHeightDiv = document.querySelector('#heightDiv');
+    var windowWidthDiv = document.querySelector('#widthDiv');
+    windowHeightDiv.innerHTML = window.innerHeight;
+    windowWidthDiv.innerHTML = window.innerWidth;
+}
