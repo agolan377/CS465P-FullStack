397cfb37470aecc9478e71f07d3dc130f17e7da7
Allen Golan <agolan@pdx.edu>
2021-02-13 11:44:50 -0800

    03-form working

--- hw3/03-form.js
+++ hw3/03-form.js
@@ -1,15 +1,41 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Add your code below
 
-app.use(/** ... */);
+app.use(express.urlencoded());
 
-app.post('/', (req, res) => {
-    // Add your code below
+app.post("/submit", (req, res) => {
+  // Add your code below
+  var message = "";
+  var newsletter = "Newsletter: ";
+  if (req.body.newsletter !== undefined) {
+    newsletter += "Yes, I would like to sign up for the newsletter.";
+  } else {
+    newsletter += "No, thank you.";
+  }
+
+  if (req.body.newsletter === undefined) {
+    message += "n/a";
+  } else {
+    message += res.body.newsletter;
+  }
+
+  res.send(
+    "Name: " +
+      req.body.name +
+      "<br/>" +
+      "Email: " +
+      req.body.email +
+      "<br/>" +
+      "Comments: " +
+      message +
+      "<br/>" +
+      newsletter
+  );
 });
-    
+
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
